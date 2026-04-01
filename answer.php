2e0544b05421342886e0bef8ae3d8f57df31534d
LohKokAku <azzrazaidan@gmail.com>
2026-04-02 02:18:38 +0700

    hesitation

--- answer.php
+++ answer.php
@@ -1,32 +1,35 @@
-<!--No. 1-->
-<!-- *Jika stok bahan kain di gudang tersedia, maka proses pemotongan pola baju dapat segera dimulai. -->
+<!--No. 1 
+*Jika stok bahan kain di gudang tersedia, maka proses pemotongan pola baju dapat segera dimulai. -->
 <?php
-$stok_kain = "Tersedia";
+$stok_kain = true;
 
-if ($stok_kain === "Tersedia") {
+if ($stok_kain) {
     echo "Proses pemotongan pola baju dapat dimulai";
 }
+echo "<hr>";
 ?>
 <!--No. 2-->
-<!-- *Jika mesin obras tidak dalam kondisi siap pakai, maka proses obras baju harus ditunda sampai mesin diperbaiki. -->
+<!-- *Jika mesin obras tidak dalam kondisi siap pakai, maka proses obras baju harus ditunda sampai mesin diperbaiki -->
 <?php
-$kondisi_mesin_obras = "Belum Tersedia";
+$kondisi_mesin_obras = false;
 
-if ($kondisi_mesin_obras === "Belum Tersedia") {
+if (!$kondisi_mesin_obras) {
     echo "Mesin obras belum siap digunakan. Perbaikan pada mesin diperlukan hingga mesin telah diperbaiki";
 }
+echo "<hr>";
 ?>
 <!--No. 3-->
 <!-- *Jika hasil jahitan baju sudah rapi dan sesuai pola, maka baju langsung dikirim ke bagian finishing selain itu, jika belum rapi, baju dikembalikan ke penjahit untuk diperbaiki terlebih dahulu. -->
 <?php
-$hasil_jahitan = "Sesuai";
+$hasil_jahitan = "sesuai";
 
-if ($hasil_jahitan === "Sesuai") {
+if ($hasil_jahitan === "sesuai") {
     echo "Hasil jahitan sesuai Pola! Mengirim ke bagian finishing";
 } 
 else {
     echo "Hasil jahitan tidak sesuai Pola, mengembalikan ke bagian penjahitan untuk diperbaiki";
 }
+echo "<hr>";
 ?>
 <!--No.4-->
 <!-- *Jika baju memiliki jahitan lepas di banyak titik dan kain robek,
@@ -41,26 +44,27 @@ maka baju dipisahkan dan dicatat sebagai produk reject ukuran.
 maka baju disetrika dan dirapikan di bagian finishing.
 *Selain itu,jika kondisi baju tidak dikenali, maka laporkan ke supervisor secara manual. -->
 <?php
-$kualitas_produksi = "Cacat Berat";
+$kualitas_produksi = "cacat berat";
 
-if ($kualitas_produksi === "Cacat Berat") {
+if ($kualitas_produksi === "cacat berat") {
     echo "Baju hasil produksi memiliki jahitan lepas di banyak titik dan kain robek. Baju dikembalikan untuk didaur ulang sebagai kain perca";
 }
-elseif ($kualitas_produksi === "Jahitan tidak rapi") {
+elseif ($kualitas_produksi === "jahitan tidak rapi") {
     echo "Baju hasil produksi memiliki jahitan tidak rapi namun kain masih utuh. Baju dikembalikan untuk diperbaiki ulang oleh bagian penjahitan";
 }
-elseif ($kualitas_produksi === "Warna kain tidak merata") {
+elseif ($kualitas_produksi === "warna tidak merata") {
     echo "Baju hasil produksi memiliki warna yang tidak merata/belang. Baju dikembalikan untuk proses ulang oleh bagian pewarnaan";
 }
-elseif ($kualitas_produksi === "Ukuran tidak sesuai") {
+elseif ($kualitas_produksi === "ukuran tidak sesuai") {
     echo "Baju hasil produksi memiliki ukuran yang tidak sesuai dengan pola yang diminta. Baju dipisahkan dan dicatat sebagai produk reject ukuran";
 }
-elseif ($kualitas_produksi === "Kusut") {
+elseif ($kualitas_produksi === "kusut") {
     echo "Baju hasil produksi dalam kondisi baik namun kusut akibat proses produksi. Baju akan dirapikan dan disetrika oleh bagian finishing";
 }
 else {
-    echo "Kondisi baju tidak dikenali, maka laporkan ke supervisor secara manual"
+    echo "Kondisi baju tidak dikenali, maka laporkan ke supervisor secara manual";
 }
+echo "<hr>";
 ?>
 <!--No. 5-->
 <!-- jenis baju adalah KEMEJA:
@@ -83,39 +87,40 @@ maka kaos dicuci, dikeringkan, dan disetrika ulang di bagian finishing.
 Selain itu, tingkat kerusakan kaos tidak dikenali,
 laporkan ke supervisor untuk pengecekan manual. -->
 <?php
-$jenis_baju = "Kemeja";
-$tingkat_kerusakan = "Berat";
+$jenis_baju = "kemeja";
+$tingkat_kerusakan = "berat";
 
-if ($jenis_baju === "Kemeja") {
+if ($jenis_baju === "kemeja") {
 
-    if ($tingkat_kerusakan === "Berat") {
+    if ($tingkat_kerusakan === "berat") {
         echo "Kemeja memiliki kerusakan berat seperti kain robek besar, obras lepas merata. Kemeja akan didaur ulang menjadi kain perca atau akan dibuang";
     }
-    elseif ($tingkat_kerusakan === "Sedang") {
+    elseif ($tingkat_kerusakan === "sedang") {
         echo "Kemeja memiliki kerusakan sedang seperti kancing copot, jahitan samping lepas. Kemeja akan dikembalikan ke bagian jahit untuk diperbaiki";
     }
-    elseif ($tingkat_kerusakan === "Ringan") {
+    elseif ($tingkat_kerusakan === "ringan") {
         echo "Kemeja memiliki kerusakan ringan seperti benang keluar sedikit, permukaan kusut. Kemeja cukup dirapikan dan disetrika di bagian finishing";
     }
     else {
         echo "Kemeja memiliki kerusakan yang tidak dikenal. Laporkan ke supervisor";
     }
 }
-elseif ($jenis_baju === "Kaos") {
+elseif ($jenis_baju === "kaos") {
 
-    if ($tingkat_kerusakan === "Berat") {
+    if ($tingkat_kerusakan === "berat") {
         echo "Kaos memiliki kerusakan berat seperti sablon mengelupas parah, kain berlubang. Kaos dimasukkan ke kategori reject dan tidak boleh dijual";
     }
-    elseif ($tingkat_kerusakan === "Sedang") {
+    elseif ($tingkat_kerusakan === "sedang") {
         echo "Kaos memiliki kerusakan sedang seperti sablon sedikit retak, jahitan leher longgar. Kaos akan dikirim ke bagian rework";
     }
-    elseif ($tingkat_kerusakan === "Ringan") {
+    elseif ($tingkat_kerusakan === "ringan") {
         echo "Kaos memiliki kerusakan ringan seperti permukaan kusut. Kaos akan dicuci dan disetrika ulang";
     }
     else {
         echo "Kaos memiliki kerusakan yang tidak dikenal. Laporkan ke supervisor";
     }
 }
+echo "<hr>";
 ?>
 <!--No. 6-->
 <!-- Jika pesanan bersifat URGENT DAN stok kain tersedia,
@@ -123,26 +128,28 @@ maka produksi langsung dimulai dan masuk jalur prioritas cepat.
 Jika pesanan bersifat URGENT DAN stok kain tidak tersedia,
 maka bagian pengadaan segera menghubungi supplier dan meminta pengiriman ekspres. -->
 <?php
-$status_pesanan = "Urgent";
-$stok_kain = "Tersedia";
+$status_pesanan = "urgent";
+$stok_kain = true;
 
-if ($status_pesanan === "Urgent" && $stok_kain === "Tersedia") {
+if ($status_pesanan === "urgent" && $stok_kain) {
     echo "Produksi akan langsung dimulai didalam jalur prioritas";
 } 
-elseif ($status_pesanan === "Urgent" && $stok_kain === "Tidak Tersedia") {
+elseif ($status_pesanan === "urgent" && !$stok_kain) {
     echo "Supplier akan dihubungi untuk pengiriman tingkat express";
 }
+echo "<hr>";
 ?>
 <!--No. 7-->
 <!-- Selama berada di area produksi jahit, jika sedang mengerjakan kemeja, kaos, jaket, atau jenis baju lainnya, seluruh karyawan wajib mengenakan seragam pabrik, celemek pelindung, dan masker sesuai standar keselamatan kerja. -->
 <?php
-$area = "Produksi";
-$jenis_baju = "Kemeja";
+$area = "produksi";
+$jenis_baju = "kemeja";
 
-if ($area === "Produksi") {
+if ($area === "produksi" && ($jenis_baju === "kemeja" || $jenis_baju === "kaos" || $jenis_baju === "jaket" || $jenis_baju === "lainnya")) {
     echo "Karyawan wajib diharuskan untuk memakai seragam pabrik, celemek pelindung, dan masker sesuai standar keselamatan pekerja";
 } 
 else {
     echo "Karyawan diperbolehkan mengenakan pakaian rapi sesuai divisi masing-masing namun tetap sopan dan sesuai peraturan perusahaan";
 }
 ?>
+<!-- so it can run in laragon -->
